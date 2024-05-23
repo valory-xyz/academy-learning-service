893eba6b5f763d9dad3b196b3f30b83fe5117802
David Vilela <dvilelaf@gmail.com>
2024-05-23 11:18:16 +0200

    fix: formatters, generators

--- packages/valory/skills/learning_chained_abci/composition.py
+++ packages/valory/skills/learning_chained_abci/composition.py
@@ -22,6 +22,7 @@
 import packages.valory.skills.learning_abci.rounds as LearningAbci
 import packages.valory.skills.registration_abci.rounds as RegistrationAbci
 import packages.valory.skills.reset_pause_abci.rounds as ResetAndPauseAbci
+import packages.valory.skills.transaction_settlement_abci.rounds as TxSettlementAbci
 from packages.valory.skills.abstract_round_abci.abci_app_chain import (
     AbciAppTransitionMapping,
     chain,
@@ -32,7 +33,6 @@ from packages.valory.skills.termination_abci.rounds import (
     Event,
     TerminationAbciApp,
 )
-import packages.valory.skills.transaction_settlement_abci.rounds as TxSettlementAbci
 
 
 abci_app_transition_mapping: AbciAppTransitionMapping = {
