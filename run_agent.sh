79bb494d165016d315717d9f231e852f85f7b058
David Vilela <dvilelaf@gmail.com>
2024-08-02 15:39:12 +0200

    fix: ensure 3rd packages hashes

--- run_agent.sh
+++ run_agent.sh
@@ -7,6 +7,12 @@ fi
 # Remove empty directories to avoid wrong hashes
 find . -empty -type d -delete
 
+# Ensure that third party packages are correctly synced
+make clean
+AUTONOMY_VERSION=v$(autonomy --version | grep -oP '(?<=version\s)\S+')
+AEA_VERSION=v$(aea --version | grep -oP '(?<=version\s)\S+')
+autonomy packages sync --source valory-xyz/open-aea:$AEA_VERSION --source valory-xyz/open-autonomy:$AUTONOMY_VERSION --update-packages
+
 # Ensure hashes are updated
 autonomy packages lock
 
